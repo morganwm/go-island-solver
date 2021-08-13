278dff96d3adae3f714f05d4d7b836f5c972fabc
Morgan Watson-Morris <morgan.wm5@gmail.com>
2021-08-13 00:12:22 -0500

    broke out into seperate file

--- island_counter.go
+++ island_counter.go
@@ -5,56 +5,6 @@ import (
 	"sync"
 )
 
-type LockableMatrix struct {
-	size        int
-	mu          sync.RWMutex
-	visitedMap  []bool
-	visitedList []struct {
-		Column int
-		Row    int
-	}
-}
-
-func NewLockableMatrix(size int) LockableMatrix {
-	return LockableMatrix{
-		size:       size,
-		visitedMap: make([]bool, size*size),
-		visitedList: []struct {
-			Column int
-			Row    int
-		}{},
-	}
-}
-
-func (l *LockableMatrix) HasVisited(column, row int) bool {
-	l.mu.RLock()
-	defer l.mu.RUnlock()
-
-	cellToCheck := (row * l.size) + column
-	if cellToCheck >= len(l.visitedMap) {
-		return false
-	}
-
-	return l.visitedMap[cellToCheck]
-}
-
-func (l *LockableMatrix) Visits(column, row int) {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-
-	l.visitedList = append(l.visitedList, struct {
-		Column int
-		Row    int
-	}{
-		Column: column,
-		Row:    row,
-	})
-
-	cellToSet := (row * l.size) + (column)
-
-	l.visitedMap[cellToSet] = true
-}
-
 func IslandCounter(topography [][]int, parallel bool) (int, []struct {
 	Column int
 	Row    int
