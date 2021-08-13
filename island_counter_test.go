1b5bed8e16c64767d2e27294e09f881359424b1f
Morgan Watson-Morris <morgan.wm5@gmail.com>
2021-08-12 23:02:13 -0500

    benchmarking

--- island_counter_test.go
+++ island_counter_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"testing"
 )
 
@@ -33,15 +32,30 @@ func TestIslandCounter(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, want2, err := IslandCounter(tt.args.topography)
+			got, _, err := IslandCounter(tt.args.topography)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("IslandCounter() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
 				t.Errorf("IslandCounter() got = %v, want %v", got, tt.want)
-				log.Printf("%+v", want2)
 			}
 		})
+	}
+}
+
+func BenchmarkIslandCounter(b *testing.B) {
+
+	testToBenchmark := tests[0]
+
+	for i := 0; i < b.N; i++ {
+		got, _, err := IslandCounter(testToBenchmark.args.topography)
+		if (err != nil) != testToBenchmark.wantErr {
+			b.Errorf("IslandCounter() error = %v, wantErr %v", err, testToBenchmark.wantErr)
+			return
+		}
+		if got != testToBenchmark.want {
+			b.Errorf("IslandCounter() got = %v, want %v", got, testToBenchmark.want)
+		}
 	}
 }
