ef035bcf175f8a86a39d6ee3169aa4f99e95a687
Morgan <mwatsonmorris@apple.com>
2023-03-06 12:59:50 -0600

    composable map changes

--- utils/composable_map.go
+++ utils/composable_map.go
@@ -1,47 +1,43 @@
 package utils
 
+import "path"
+
 type ComposableMap[T any] struct {
-	defaultVal    T
-	directMapping map[string]T
-	nestedMapping map[string]ComposableMap[T]
+	defaultVal T
+	mapping    map[string]T
 }
 
-func (c *ComposableMap[T]) Get(key string) (out T, ok bool) {
-	ok = false
-
-	if key == "" {
-		return c.defaultVal, true
+func (c *ComposableMap[T]) GetKeys() (out []string) {
+	for k := range c.mapping {
+		out = append(out, k)
 	}
-
-	if val, ok := c.directMapping[key]; ok {
-		return val, ok
-	}
-
-	for _, val := range c.nestedMapping {
-		if v, ok := val.Get(key); ok {
-			return v, ok
-		}
-	}
-
 	return
 }
 
-func (c *ComposableMap[T]) SetDefault(val T) {
-	c.defaultVal = val
+func (c *ComposableMap[T]) GetDefault() T {
+	return c.defaultVal
 }
 
-func (c *ComposableMap[T]) SetDirect(key string, val T) {
-	c.directMapping[key] = val
+func (c *ComposableMap[T]) GetMapping() map[string]T {
+	return c.mapping
 }
 
-func (c *ComposableMap[T]) SetNested(key string, val ComposableMap[T]) {
-	c.nestedMapping[key] = val
+func (c *ComposableMap[T]) Set(key string, val T) {
+	c.mapping[key] = val
 }
 
-func (c *ComposableMap[T]) ReplaceDirect(in map[string]T) {
-	c.directMapping = in
+func (c *ComposableMap[T]) Get(key string) (out T) {
+	if val, ok := c.mapping[key]; ok {
+		return val
+	}
+
+	return c.GetDefault()
 }
 
-func (c *ComposableMap[T]) ReplaceNested(in map[string]ComposableMap[T]) {
-	c.nestedMapping = in
+func (c *ComposableMap[T]) Add(prefix string, in ComposableMap[T]) {
+	c.Set(prefix, in.GetDefault())
+
+	for k, v := range in.GetMapping() {
+		c.Set(path.Join(prefix, k), v)
+	}
 }
